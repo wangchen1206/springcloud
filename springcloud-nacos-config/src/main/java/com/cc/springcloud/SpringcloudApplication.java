3a2fe03e6cda1d327d2fb57e02b53e9a622d89f4
chen-wang1 <chen.wang1@hp.com>
2019-10-23 20:39:22 +0800

    update

--- springcloud-nacos-config/src/main/java/com/cc/springcloud/SpringcloudApplication.java
+++ springcloud-nacos-config/src/main/java/com/cc/springcloud/SpringcloudApplication.java
@@ -1,15 +1,8 @@
 package com.cc.springcloud;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @Author cc
