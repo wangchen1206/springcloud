e4e711bc0cee2288d9dca2fa483e44666ab3c7ca
13027730830 <wangchen1@zhongguancun.com.cn>
2020-07-11 10:59:37 +0800

    add mybatis-plus tests

--- spring-demo/src/test/java/com/cc/spring/ApplicationTests.java
+++ spring-demo/src/test/java/com/cc/spring/ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.cc.spring;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cc.spring.entity.User;
+import com.cc.spring.service.IUserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private IUserService userService;
+
     @Test
     void contextLoads() {
+        System.out.println(11);
+        User cc = userService.getOne(new QueryWrapper<User>().lambda().eq(User::getUsername, "cc"));
+        System.out.println(cc.getUsername()+"---"+cc.getUserInfor());
+
     }
 
 }
