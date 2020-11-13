ba094012bff95cc0dde66fe9c4bce8633a560614
13027730830 <wangchen1@zhongguancun.com.cn>
2020-11-13 16:23:27 +0800

    增加 并发队列 集合 锁 测试

---
+++ spring-demo/src/main/java/com/cc/spring/concurrent/HashMapDemo.java
@@ -0,0 +1,20 @@
+package com.cc.spring.concurrent;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+ 
+    public static void main(String[] args) {
+        HashMap map = new HashMap<HashMapDemo,Integer>(1);
+        for (int i = 0; i < 1000; i++) {
+            HashMapDemo hashMapDemo1 = new HashMapDemo();
+            map.put(hashMapDemo1, null);
+        }
+        System.out.println("运行结束");
+    }
+ 
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
